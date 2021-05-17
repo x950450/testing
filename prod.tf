8cbb59d6f7c9230cbe13b49ca26c84423ff90f61
x950450 <ksujeesh0080@gmail.com>
2021-05-17 15:52:30 +0000

    second

--- prod.tf
+++ prod.tf
@@ -16,3 +16,68 @@ resource "azurerm_storage_account" "Storage1" {
   }
 }
 
+
+
+resource "azurerm_virtual_network" "avent1" {
+  name                = "avent1"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.RG1.location}"
+  resource_group_name = "${azurerm_resource_group.RG1.name}"
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subent1"
+  resource_group_name  = "${azurerm_resource_group.RG1.name}"
+  virtual_network_name = "${azurerm_virtual_network.avent1.name"
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic001" {
+  name                = "nic001"
+  location            = "${azurerm_resource_group.RG1.location}"
+  resource_group_name = "${azurerm_resource_group.RG1.name}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm0001" {
+  name                  = "vm0001"
+  location              = "${azurerm_resource_group.RG1.location}"
+  resource_group_name   = "${azurerm_resource_group.RG1.name}"
+  network_interface_ids = [azurerm_network_interface.nic001.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "adminadmin"
+    admin_password = "Warrior@12345"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
